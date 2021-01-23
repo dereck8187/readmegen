1b8de04b9f47796bd0554a8a0c06089b6803f943
viktor vaughn <dereck8187@gmail.com>
2021-01-23 10:40:19 -0500

    working on js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,15 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} 
+  return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
 `;
+
+${data.usage}
+
+${data.description}
+
+${renderLicenseLink(data.license)}
 }
 
 module.exports = generateMarkdown;
