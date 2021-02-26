039be9f3671c5835f8b200c78cfa9a634c60517d
viktor vaughn <dereck8187@gmail.com>
2021-02-26 12:01:39 -0500

    i have no idea

--- index.js
+++ index.js
@@ -85,3 +85,5 @@ async function init() {
   }
   
   init();  
+
+  module.exports = functionname
--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -1,4 +1,6 @@
 // generateReadme function populating the README.md
+
+
 function generateReadme(answers) {
     return `
 <h1 align="center">${answers.projectTitle} 👋</h1>
